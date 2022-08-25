6c129bf3a3706ae104fc79031e24f7bc15858743
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-24 23:32:49 -0700

    Hello World! API, is working

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}, () => { 
+    console.log('connected to mongo: ', 
+    process.env.MONGO_URI) }
 )
 
 // MIDDLEWARE
@@ -17,6 +20,11 @@ app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
+
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 
 // LISTEN
 app.listen(PORT, () => {
